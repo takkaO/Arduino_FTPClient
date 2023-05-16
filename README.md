75adb2798bb288a8cc26fc11dc361882e1bcc7a2
takkaO <46308252+takkaO@users.noreply.github.com>
2023-05-16 18:00:49 +0900

    [update] test ok

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# FTP Client for the ESP32
+# FTP Client for the Arduino
 
-A FTP-Client for the ESP32
+A FTP-Client for the Arduino
 
 [![GitHub version](https://img.shields.io/github/release/ldab/ESP32_FTPClient.svg)](https://github.com/ldab/ESP32_FTPClient/releases/latest)
 [![Build Status](https://travis-ci.org/ldab/ESP32_FTPClient.svg?branch=master)](https://travis-ci.org/ldab/ESP32_FTPClient)
@@ -29,7 +29,7 @@ A FTP-Client for the ESP32
 * I was only able to download files up to ~90KB, as I can't see any usage for that, I did not spend extra time, as it's partially working.
 * A new example under [download_file.ino](./examples/donload_file/download_file.ino) has been added;
 * `ESP.getMaxAllocHeap()` returned limited amount of free memory, therefore the buffer can't hold the entire downloaded file.
-  * For this reason I have implemented the last argument of `DownloadFile(const char * filename, unsigned char * buf, size_t length, bool printUART )` which will print the buffer directly to the UART;
+  * For this reason I have implemented the last argument of `downloadFile(const char * filename, unsigned char * buf, size_t length, bool printUART )` which will print the buffer directly to the UART;
   * If required, SPIFFS can be used to save the file.
 
 As mentioned [here](https://github.com/espressif/arduino-esp32/issues/1163) and [here](https://github.com/espressif/esp-idf/issues/3497)
--- examples/upload_image/upload_image.ino
+++ examples/upload_image/upload_image.ino
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-ESP32-CAM remote image access via FTP. Take pictures with ESP32 and upload it via FTP making it accessible for the outisde network. 
+ESP32-CAM remote image access via FTP. Take pictures with ESP32 and upload it via FTP making it accessible for the outisde network.
 Leonardo Bispo
 July - 2019
 https://github.com/ldab/ESP32_FTPClient
@@ -9,73 +9,74 @@ Distributed as-is; no warranty is given.
 
 ******************************************************************************/
 #include "Arduino.h"
-#include <WiFi.h>
-#include <WiFiClient.h> 
-#include <ESP32_FTPClient.h>
 #include "octocat.h"
+#include <Arduino_FTPClient.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
 
-#define WIFI_SSID ""
-#define WIFI_PASS ""
+#define WIFI_SSID "ASUS_image"
+#define WIFI_PASS "0236443222"
 
-char ftp_server[] = "";
-char ftp_user[]   = "";
-char ftp_pass[]   = "";
+char ftp_server[] = "192.168.0.220";
+char ftp_user[]   = "admin";
+char ftp_pass[]   = "admin";
 
 // you can pass a FTP timeout and debbug mode on the last 2 arguments
-ESP32_FTPClient ftp (ftp_server,ftp_user,ftp_pass, 5000, 2);
+Arduino_FTPClient ftp(ftp_server, ftp_user, ftp_pass, 5000, 2);
+WiFiClient client;
+WiFiClient data_client;
 
-void setup()
-{
-  Serial.begin( 115200 );
+void setup() {
+	Serial.begin(115200);
+	ftp.setClients(client, data_client);
+	//ftp.setClient(client);
+	//ftp.setDataClient(data_client);
 
-  WiFi.begin( WIFI_SSID, WIFI_PASS );
-  
-  Serial.println("Connecting Wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+	WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-  ftp.OpenConnection();
+	Serial.println("Connecting Wifi...");
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
 
-  // Get directory content
-  ftp.InitFile("Type A");
-  String list[128];
-  ftp.ChangeWorkDir("/public_html/zyro/gallery_gen/");
-  ftp.ContentList("", list);
-  Serial.println("\nDirectory info: ");
-  for(int i = 0; i < sizeof(list); i++)
-  {
-    if(list[i].length() > 0)
-      Serial.println(list[i]);
-    else
-      break;
-  }
+	ftp.openConnection();
 
-  // Make a new directory
-  ftp.InitFile("Type A");
-  ftp.MakeDir("my_new_dir");
+	// Get directory content
+	// ftp.initFile("Type A");
+	// String list[128];
+	// ftp.changeWorkDir("/public_html/zyro/gallery_gen/");
+	// ftp.contentList("", list);
+	// Serial.println("\nDirectory info: ");
+	// for (int i = 0; i < sizeof(list); i++) {
+	// 	if (list[i].length() > 0)
+	// 		Serial.println(list[i]);
+	// 	else
+	// 		break;
+	// }
 
-  // Create the new file and send the image
-  ftp.ChangeWorkDir("my_new_dir");
-  ftp.InitFile("Type I");
-  ftp.NewFile("octocat.jpg");
-  ftp.WriteData( octocat_pic, sizeof(octocat_pic) );
-  ftp.CloseFile();
+	// Make a new directory
+	ftp.initFile("Type A");
+	ftp.makeDir("my_new_dir");
 
-  // Create the file new and write a string into it
-  ftp.InitFile("Type A");
-  ftp.NewFile("hello_world.txt");
-  ftp.Write("Hello World");
-  ftp.CloseFile();
+	// Create the new file and send the image
+	ftp.changeWorkDir("my_new_dir");
+	ftp.initFile("Type I");
+	ftp.newFile("octocat.jpg");
+	ftp.writeData(octocat_pic, sizeof(octocat_pic));
+	ftp.closeFile();
 
-  ftp.CloseConnection();
+	// Create the file new and write a string into it
+	ftp.initFile("Type A");
+	ftp.newFile("hello_world.txt");
+	ftp.write("Hello World");
+	ftp.closeFile();
+
+	ftp.closeConnection();
 }
 
-void loop()
-{
-
+void loop() {
 }
